5b82984612ae594a6eacba48cc7a55f4bb6cc16e
Srikanthbolle <bollesrikanth186@gmail.com>
2025-06-04 23:46:29 +0530

    Create Add Product Page

--- src/app/Admin/AddProduct/page.tsx
+++ src/app/Admin/AddProduct/page.tsx
@@ -1,10 +1,69 @@
-'use client'
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [image, setImage] = useState();
+  const [data,setData] = useState({
+    title:"",
+    description:"",
+    category:"Startup",
+    author:"Alex Bennett",
+    autorImg:"",
 
-export default page
+  })
+  return (
+    <>
+      <form action="" className="px-8 pt-10">
+        <p className="mb-2">Upload Thumbnail</p>
+        <label htmlFor="image">
+          <Image
+            src={!image ? "/assets/upload.jpg" : URL.createObjectURL(image)}
+            width={140}
+            height={20}
+            alt="upload_image"
+            className="border border-dashed border-gray-300"
+          />
+        </label>
+        <input
+          onChange={(e: any) => setImage(e.target.files[0])}
+          type="file"
+          id="image"
+          hidden
+          required
+        />
+        <p className="mt-8 mb-2 text-xl">Blog Title</p>
+        <input
+          type="text"
+          placeholder="Type here"
+          required
+          className="px-3 py-1 w-full max-w-[500px] border rounded-md"
+        />
+
+        <p className="mt-8 mb-2 text-xl">Blog Description</p>
+        <textarea
+          type="text"
+          rows={6}
+          placeholder="Write content"
+          required
+          className="px-3 py-1 w-full max-w-[500px] border rounded-md"
+        />
+        <p className="mt-8 mb-2 text-xl">Blog Category</p>
+        <select
+          name="category"
+          className="w-40 mt-4 px-4 py-3 border text-gray-500"
+        >
+          <option value="Startup">Startup</option>
+          <option value="Technology">Technology</option>
+          <option value="Lifestyle">Lifestyle</option>
+        </select>
+        <br />
+        <button type="submit" className="mt-8 w-40 h-12 bg-black text-white">
+          Add
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default page;
--- src/app/Admin/layout.tsx
+++ src/app/Admin/layout.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import Sidebar from '../../../Components/AdminComponents/Sidebar'
+import React from "react";
+import Sidebar from "../../../Components/AdminComponents/Sidebar";
+import Image from "next/image";
 
-export default function Layout ({children}) {
+export default function Layout({ children }) {
   return (
     <>
-    <div className='flex'>
-    <Sidebar />
-    </div>
-    <div>{children}</div>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-between w-full py-4 px-5 border-b-gray-400 border-b-2">
+            <h3>Admin Panel</h3>
+            <Image
+              src="/assets/profile_1.png"
+              width={30}
+              height={30}
+              alt="profile_image"
+            />
+          </div>
+           {children}
+        </div>
+       
+      </div>
+      
     </>
-  )
+  );
 }
-
